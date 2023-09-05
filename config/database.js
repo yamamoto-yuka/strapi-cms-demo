6ea53922871802a08f0b6890323fe633919e5605
Yuka Yamamoto <yamamoto_yuka@hotmail.com>
2023-09-05 23:08:07 +0900

    modify config

--- config/database.js
+++ config/database.js
@@ -96,7 +96,7 @@ module.exports = ({ env }) => ({
     client: 'mysql',
     connection: {
 
-      host: env('DATABASE_HOST', '139.59.102.235/'),
+      host: env('DATABASE_HOST', '139.59.102.235'),
       port: env.int('DATABASE_PORT', 3306),
       database: env('DATABASE_NAME', 'strapi'),
       user: env('DATABASE_USERNAME', 'strapi'),
